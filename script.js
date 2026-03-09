2a60d0792c1e8c605156fdf79c6a8a6c0df3e9e2
bishal10rm <talukdarbishal68@gmail.com>
2026-03-09 20:50:38 +0600

    Added  modal

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 
 let allIssues =[];
 const cardsContainer=document.getElementById("cardsContainer");
+const loadingSpinner=document.getElementById("loadingSpinner");
+const issues_modal=document.getElementById("issues_modal");
+const issueTitle=document.getElementById("issueTitle");
+const issueStatus=document.getElementById("issueStatus");
+const issueAuthor=document.getElementById("issueAuthor");
+const issueDate=document.getElementById("issueDate");
+const issueDescription=document.getElementById("issueDescription");
+const issueAssignee=document.getElementById("issueAssignee");
+const issuePriority=document.getElementById("issuePriority");
+const issueName=document.getElementById("issueName");
 
 async function loadIssues(){
+   loadingSpinner.classList.remove("hidden");
    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
    const issue = await res.json();
 
    allIssues =issue.data;
    displayIssues(allIssues);
-}
+}   
+   
+
 function displayIssues(datas){
+   
    cardsContainer.innerHTML="";
    document.getElementById("issueCount").innerText=datas.length+" Issues"
      datas.forEach((data)=>{
@@ -26,11 +40,12 @@ function displayIssues(datas){
       </div>
 
       <div class="p-5">
-        <h3 class="text-[14px] font-semibold">${data.title}</h3>
+        <h3 class="text-[14px] font-semibold"onclick="openIssueModal(${data.id})">${data.title}</h3>
         <p class="text-[12px]">${data.description}</p>
 
         ${data.labels.map((label) =>`<div class="badge badge-soft badge-warning">${label}</div>`)
           .join("")}
+          
 
         <br><br><hr>
       </div>
@@ -42,11 +57,30 @@ function displayIssues(datas){
 
     </div>
     `;
-
+    card.onclick = () => (data.id);
     cardsContainer.appendChild(card);
   });
 }
 
+async function openIssueModal(dataid){
+   console.log(dataid,"dataid");
+   const res= await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${dataid}`);
+   const data = await res.json();
+   const issueDetails=data.data;
+   console.log(data,"data");
+   console.log(issueDetails,"data")
+   issues_modal.showModal();
+   
+   issueTitle.textContent=issueDetails.title;
+   issueStatus.textContent=issueDetails.status;
+   issueAuthor.textContent=issueDetails.aurhor;
+   issueDate.textContent=issueDetails.date;
+   issueDescription.textContent=issueDetails.description;
+   issueAssignee.textContent=issueDetails.assignee;
+   issueName.textContent=issueDetails.Name;
+   issuePriority.textContent=issueDetails.priority;
+
+}
 loadIssues();
 
 document.getElementById("allTab").addEventListener("click",()=>{
